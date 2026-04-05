2e1a56652ce7412191b3d4d2d9d9adb91e26c385
vivek suryawanshi <viveklsuryawanshi@gmail.com>
2026-04-05 07:55:53 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -2,26 +2,9 @@ const express = require("express");
 
 const app = express();
 
-app.use("/user", (req, res) => {
-  res.send({ message: "HAHAHA" });
-});
-
-app.get("/user", (req, res) => {
+app.get("/user/:userId", (req, res) => {
+  console.log(req.params.userId);
   res.send({ firstName: "Vivek", lastName: "Suryawanshi" });
-});
-
-app.post("/user", (req, res) => {
-  console.log(" data saved to database successfully");
-  res.send({ message: "User data saved successfully" });
-});
-
-app.delete("/user", (req, res) => {
-  console.log("User data deleted from database successfully");
-  res.send({ message: "User data deleted successfully" });
-});
-
-app.use("/test", (req, res) => {
-  res.send("Hello from server!");
 });
 
 app.listen(3000, () => {
