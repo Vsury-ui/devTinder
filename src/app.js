26186e413d5ad2a819b1b9a92b87465ed0fec09e
vivek suryawanshi <viveklsuryawanshi@gmail.com>
2026-04-11 05:57:55 +0530

    Handling multiple routes

--- src/app.js
+++ src/app.js
@@ -2,10 +2,39 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:userId", (req, res) => {
-  console.log(req.params.userId);
-  res.send({ firstName: "Vivek", lastName: "Suryawanshi" });
-});
+// app.use(
+//   "/user",
+//   (req, res) => {
+//     console.log("User route accessed");
+//     res.send("User routed");
+//   });
+
+app.use("/user", [
+  (req, res, next) => {
+    console.log("User route accessed");
+    next();
+    //res.send("User routed");
+  },
+  (req, res, next) => {
+    console.log("Second middleware for /user");
+    //res.send("Second middleware response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Third middleware for /user");
+    //res.send("Third middleware response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Fourth middleware for /user");
+    // res.send("Fourth middleware response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Final handler for /user");
+    res.send("User routed");
+  },
+]);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
