1479d634d425d474b7930a07dd325fb40ff39723
vivek suryawanshi <viveklsuryawanshi@gmail.com>
2026-04-18 17:27:34 +0530

    API level validations

--- src/app.js
+++ src/app.js
@@ -2,10 +2,51 @@ const express = require("express");
 const { connectDB } = require("./config/database");
 
 const User = require("./models/user");
+const user = require("./models/user");
 
 const app = express();
 //express middleware to parse JSON request bodies
 app.use(express.json());
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.email;
+
+  try {
+    const user = await User.findOne({ email: userEmail });
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    console.error("Error fetching user", err);
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.get("/user/:id", async (req, res) => {
+  const userId = req.params.id;
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    console.error("Error fetching user", err);
+    res.status(400).send("Something went wrong");
+  }
+});
 
 app.post("/signup", async (req, res) => {
   // create a new user document and save it to the database
@@ -14,11 +55,55 @@ app.post("/signup", async (req, res) => {
 
   const user = new User(req.body);
   try {
+    if (user.skills.length > 10) {
+      throw new Error("You can add maximum 10 skills");
+    }
+    if (user.photoUrl && !user.photoUrl.startsWith("http")) {
+      throw new Error("Photo URL must be a valid URL");
+    }
     await user.save();
     res.send("User created successfully");
   } catch (err) {
     console.error("Error creating user", err);
     res.status(400).send("Internal Server Error");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+
+  try {
+    await User.findByIdAndDelete(userId);
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const updateData = req.body;
+  try {
+    const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+
+    const isUpdateAllowed = Object.keys(updateData).every((key) =>
+      ALLOWED_UPDATES.includes(key),
+    );
+    if (!isUpdateAllowed) {
+      throw new Error("Update contains invalid fields");
+    }
+    if (updateData.skills.length > 10) {
+      throw new Error("You can add maximum 10 skills");
+    }
+    if (updateData.photoUrl && !updateData.photoUrl.startsWith("http")) {
+      throw new Error("Photo URL must be a valid URL");
+    }
+    await User.findOneAndUpdate({ _id: userId }, updateData, {
+      runValidators: true,
+    });
+    res.send("User updated successfully");
+  } catch (err) {
+    res.status(400).send("UPDATE FAILED:  " + err.message);
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,58 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+      minLength: 4,
+      maxLength: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (["male", "female", "other"].indexOf(value) === -1) {
+          throw new Error("Gender must be either male, female or other");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+    },
+    about: {
+      type: String,
+      default:
+        "This is default about section. Please update it to tell others about yourself.",
+    },
+    skills: {
+      type: [String],
+      array: true,
+    },
   },
-  lastName: {
-    type: String,
-  },
-  email: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  { timestamps: true },
+);
 
 module.exports = mongoose.model("User", userSchema);
